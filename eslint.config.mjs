7c29d58463ffd1ff38ffec7d1f939c88625a8baf
Nadya <nb1985@ukr.net>
2025-07-10 14:51:18 +0300

    chore(config): add initial configuration for Next.js and ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,53 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/**  import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+export default defineConfig([*/
+  //{ files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  //{ files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+ /* tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]); */
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+
+
+
+
+import { defineConfig, FlatCompat } from 'eslint/config'
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname })
+
+export default defineConfig([
+  { root: true }, 
+  { ignores: ['**/node_modules/**', 'dist/**'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    ...tseslint.configs.recommended,
+    languageOptions: {
+      parserOptions: { project: './tsconfig.json' }, 
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    languageOptions: { globals: globals.browser },
+  },
+  ...compat.config({ extends: ['next', 'next/core-web-vitals'] }),
+]);
+
